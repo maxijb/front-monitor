dbf346ed354b6cfc1529f853c1a74828febb345d
mbenedetto <mbenedetto@despegar.com>
2013-10-28 17:19:25 -0300

    Try catch en grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -123,17 +123,20 @@ module.exports = function (grunt) {
 
   // Get path to core grunt dependencies from Sails
   var depsPath = grunt.option('gdsrc') || 'node_modules/sails/node_modules';
-//  grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
-  grunt.loadTasks('node_modules/grunt-contrib-uglify/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-less/tasks');
-//  grunt.loadTasks(depsPath + '/grunt-contrib-coffee/tasks');
+  try {
+	  grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-less/tasks');
+	  grunt.loadTasks(depsPath + '/grunt-contrib-coffee/tasks');
+  } catch (e) {
+	  grunt.loadTasks('node_modules/grunt-contrib-uglify/tasks');
+  }
 
   // Project configuration.
   grunt.initConfig({
